7381d1716e511248a196004ef946feae6ff51670
191229-harika <85675503+191229-harika@users.noreply.github.com>
2022-06-26 20:26:50 +0530

    Create sqlite.java

---
+++ Mobile Application Development Lab/SQLite/sqlite.java
@@ -0,0 +1,168 @@
+//Sqlite
+
+//MAinActivity.java
+
+package com.example.sqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    DatabaseHelper databaseHelper;
+    EditText et_username, et_password, et_cpassword;
+    Button btn_register, btn_login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        databaseHelper = new DatabaseHelper(this);
+        et_username = (EditText)findViewById(R.id.et_username);
+        et_password = (EditText)findViewById(R.id.et_password);
+        et_cpassword = (EditText)findViewById(R.id.et_cpassword);
+        btn_register = (Button)findViewById(R.id.btn_register);
+        btn_login = (Button)findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentSignUP=new Intent(getApplicationContext(),Login.class);
+                startActivity(intentSignUP);
+            }
+        });
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = et_username.getText().toString();
+                String password = et_password.getText().toString();
+                String confirm_password = et_cpassword.getText().toString();
+                if(username.equals("") || password.equals("") || confirm_password.equals("")){
+                    Toast.makeText(getApplicationContext(), "Fields Required", Toast.LENGTH_SHORT).show();
+                }else{
+                    if(password.equals(confirm_password)){
+
+                        Boolean checkusername = databaseHelper.CheckUsername(username);
+                        if(checkusername == true){Boolean insert = databaseHelper.Insert(username, password);
+                            if(insert == true){
+                                Toast.makeText(getApplicationContext(), "Registered", Toast.LENGTH_SHORT).show();
+                                et_username.setText("");
+                                et_password.setText("");
+                                et_cpassword.setText("");
+                            }
+                        }else{
+                            Toast.makeText(getApplicationContext(), "Username already taken", Toast.LENGTH_SHORT).show();
+                        }
+                    }else{
+                        Toast.makeText(getApplicationContext(), "Password does not match", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+        }
+    }
+
+
+// DBHelper.java
+
+package com.example.sqlite;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+class DatabaseHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "login.db";
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE user(ID INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT)");
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS user");
+    } public boolean Insert(String username, String password){
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("username", username);
+        contentValues.put("password", password);
+        long result = sqLiteDatabase.insert("user", null, contentValues);
+        if(result == -1){
+            return false;
+        }else{
+            return true;}}
+    public Boolean CheckUsername(String username){
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM user WHERE username=?", new String[]{username});
+        if(cursor.getCount() > 0){
+            return false;
+        }else{
+            return true;
+        }}
+    public Boolean CheckLogin(String username, String password){
+        SQLiteDatabase sqLiteDatabase = this.getReadableDatabase();
+        Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM user WHERE username=? AND password=?", new String[]{username, password});
+        if(cursor.getCount() > 0){
+            return true;
+        }else{
+            return false;
+        }}}
+
+
+//Login.java
+
+package com.example.sqlite;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login extends AppCompatActivity {
+    Button  btn_llogin;
+    EditText et_lusername, et_lpassword;
+
+    DatabaseHelper databaseHelper;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        databaseHelper = new DatabaseHelper(this);
+        et_lusername = (EditText)findViewById(R.id.username);
+        et_lpassword = (EditText)findViewById(R.id.Password);
+        btn_llogin = (Button)findViewById(R.id.Login);
+
+        btn_llogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String username = et_lusername.getText().toString();
+                String password = et_lpassword.getText().toString();
+
+                Boolean checklogin = databaseHelper.CheckLogin(username, password);
+                if(checklogin == true){
+                    Toast.makeText(getApplicationContext(), "Login Successful", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(), "Invalid username or password",
+
+                            Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+    }}
+
+
+
